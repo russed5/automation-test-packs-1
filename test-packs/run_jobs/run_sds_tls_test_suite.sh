ce36188d000a8bfc1f7353d88e1c47315461b756
Ragubathy Jayaraju <r.jayaraju@dell.com>
2017-11-14 15:10:32 +0530

    ESTS-137472 - SDS TLS enabled test

---
+++ test-packs/run_jobs/run_sds_tls_test_suite.sh
@@ -0,0 +1,11 @@
+# Copyright © 2017 Dell Inc. or its subsidiaries.  All Rights Reserved
+TEST_SUITE_NAME='Test Automation Framework MVP'
+REPORT_NAME='run_sds_tls_test_suite_report'
+SEARCH_PATH=$AF_TEST_SUITE_PATH/
+MARKER='sds_tls_enabled'
+
+. $HOME/af_env.sh
+py3clean .
+export AF_TEST_SUITE_NAME=$TEST_SUITE_NAME
+py.test $SEARCH_PATH -m "$MARKER" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
+export AF_TEST_SUITE_NAME='Test Suite Name Not Set'
---
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/c_SystenDefinition/test_e_systemDefinition_tls.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Author: Shane McGowan
+# Revision: 1.0
+# Code Reviewed by: Toqeer Akhtar
+# Description: Testing the Converged System Components Rest API V's JSON input file
+#
+# Copyright (c) 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+# Dell EMC Confidential/Proprietary Information
+
+import time
+import sys
+import af_support_tools
+import pytest
+import string
+import requests
+import json
+import os
+
+
+
+@pytest.mark.sds_tls_enabled
+def test_sds_consul():
+    """
+            Title: sds Registered in con
+            Description: This test verify SystemDefinition Services is running in TLS protocol
+            Params: None
+            Returns: None
+        """
+    err = []
+
+
+    consul_url = 'https://ragu89.mpe.lab.vce.com:8088/about'
+    resp = requests.get(consul_url, verify= '/usr/local/share/ca-certificates/taf.cpsd.dell.ca.crt')
+    data = json.loads(resp.text)
+
+    assert resp.status_code == 200, "Request has not been acknowledged as expected."
+
