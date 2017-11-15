3b2d9df90b4bc62bb4df24deda6c0c5d98c6acd7
Ragubathy Jayaraju <r.jayaraju@dell.com>
2017-11-15 14:12:21 +0530

    ESTS-137472 - updated script to execute sds test cases

--- test-packs/run_jobs/run_sds_tls_test_suite.sh
+++ test-packs/run_jobs/run_sds_tls_test_suite.sh
@@ -1,11 +1,5 @@
 # Copyright © 2017 Dell Inc. or its subsidiaries.  All Rights Reserved
-TEST_SUITE_NAME='Test Automation Framework MVP'
-REPORT_NAME='run_sds_tls_test_suite_report'
-SEARCH_PATH=$AF_TEST_SUITE_PATH/
-MARKER='sds_tls_enabled'
-
 . $HOME/af_env.sh
 py3clean .
-export AF_TEST_SUITE_NAME=$TEST_SUITE_NAME
-py.test $SEARCH_PATH -m "$MARKER" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
-export AF_TEST_SUITE_NAME='Test Suite Name Not Set'
+export AF_TEST_SUITE_NAME='System definition service test'
+python /home/autouser/PycharmProjects/auto-framework/test_suites/system_definition_service/test_system_definition_a_ConvergedSystemAdditionRequestedAMQP_Negative.py
