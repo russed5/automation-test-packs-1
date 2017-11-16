369b2121926b0eceaf24adf1b5b903ab3839f041
Aoife Cullinane <aoife.cullinane@vce.com>
2017-11-16 09:01:00 +0000

    ESTS-136560: Update for TLS changes

--- test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_iz_scanLogFilesForPasswords.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_iz_scanLogFilesForPasswords.py
@@ -19,26 +19,6 @@ def load_test_data():
     import cpsd
     global cpsd
 
-    # Update config ini files at runtime
-    my_data_file = os.environ.get('AF_RESOURCES_PATH') + '/continuous-integration-deploy-suite/symphony-sds.properties'
-    af_support_tools.set_config_file_property_by_data_file(my_data_file)
-
-    # Set config ini file name
-    global env_file
-    env_file = 'env.ini'
-
-    # Test VM Details
-    global ipaddress
-    ipaddress = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='hostname')
-
-    global cli_username
-    cli_username = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
-                                                             property='username')
-
-    global cli_password
-    cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
-                                                             property='password')
-
     # Get the typically used passwords
     global setup_config_file
     setup_config_file = 'continuous-integration-deploy-suite/setup_config.ini'
@@ -62,58 +42,96 @@ def load_test_data():
                                                              property='vcenter_password_fra')
 
 
+@pytest.fixture()
+def setup():
+    parameters = {}
+    env_file = 'env.ini'
+    ipaddress = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
+                                                          property='hostname')
+    parameters['IP'] = ipaddress
+    cli_user = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
+                                                         property='username')
+    parameters['cli_user'] = cli_user
+    cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
+                                                             property='password')
+    parameters['cli_password'] = cli_password
+    return parameters
+
+
 #####################################################################
 # These are the main tests.
 #####################################################################
 
-@pytest.mark.parametrize('filePath, infoLogFile',
-                            [('/opt/dell/cpsd/rackhd-adapter/logs/', 'rackhd-adapter-info.log'),
-                            ('/opt/dell/cpsd/vcenter-adapter/logs/', 'vcenter-adapter-info.log'),
-                            ('/opt/dell/cpsd/scaleio-adapter/logs/', 'scaleio-adapter-info.log'),
-                            ('/opt/dell/cpsd/dne-paqx/logs/', 'dne-paqx-info.log'),
-                            ('/opt/dell/cpsd/node-discovery-paqx/logs/', 'node-discovery-info.log'),
-                            ('/opt/dell/cpsd/ess/logs/', 'ess-info.log')])
+@pytest.mark.parametrize('service, directory', [("dell-cpsd-hal-vcenter-adapter", "vcenter-adapter"),
+                                                ("dell-cpsd-hal-rackhd-adapter", "rackhd-adapter"),
+                                                ("dell-cpsd-hal-scaleio-adapter", "scaleio-adapter"),
+                                                ("symphony-dne-paqx", "dne-paqx"),
+                                                ("symphony-engineering-standards-service", "ess"),
+                                                ("dell-cpsd-node-discovery-paqx", "node-discovery-paqx"),
+                                                ("dell-cpsd-core-credential-service", "credential"),
+                                                ("dell-cpsd-core-endpoint-registration-service",
+                                                 "registration-services/endpoint-registration")])
 @pytest.mark.daily_status
 @pytest.mark.dne_paqx_parent_mvp
 @pytest.mark.dne_paqx_parent_mvp_extended
-def test_dne_log_files_free_of_passwords(filePath, infoLogFile):
-    """
-    Description     :       This method tests that the log files have none of the common Lab passwords.
-                            It will fail:
-                                If the info log files has any plain text passwords.
-    Parameters      :       File path & log file name
-    Returns         :       None
-    """
+def test_dne_services_log_files_passwords(service, directory, setup):
+    filePath = '/opt/dell/cpsd/' + directory + '/logs/'
+
+    infoLogFile = directory + '-info.log'
+
+    # Need this exception as the node-discovery-paqx log file format is different to the others
+    if filePath == '/opt/dell/cpsd/node-discovery-paqx/logs/':
+        infoLogFile = 'node-discovery-info.log'
+
+    if filePath == '/opt/dell/cpsd/credential/logs/':
+        infoLogFile = 'cpsd-credentials-service-info.log'
+
+    if filePath == '/opt/dell/cpsd/registration-services/endpoint-registration/logs/':
+        infoLogFile = 'endpoint-registration-service-info.log'
+
+    # Verify the log files exist
+    sendCommand = 'docker ps | grep ' + service + ' | awk \'{system("docker exec -i "$1" ls ' + filePath + '") }\''
+
+    my_return_status = af_support_tools.send_ssh_command(host=setup['IP'], username=setup['cli_user'],
+                                                         password=setup['cli_password'],
+                                                         command=sendCommand, return_output=True)
+    print(my_return_status)
+    error_list = []
+
+    if (infoLogFile not in my_return_status):
+        error_list.append(infoLogFile)
 
     password1 = fra_password
     password2 = rtp_password
     password3 = rackHD_password
 
-    error_list = []
-
-    # Verify there are no plaintext passwords
-    sendCommand = 'cat ' + filePath + infoLogFile + ' | grep \'' + password1 + '\''
-    my_return_status = af_support_tools.send_ssh_command(host=ipaddress, username=cli_username, password=cli_password,
+    # Verify there are no Password #1
+    sendCommand = 'docker ps | grep ' + service + ' | awk \'{system("docker exec -i "$1" cat ' + filePath + infoLogFile + ' | grep ' + password1 + '")}\''
+    my_return_status = af_support_tools.send_ssh_command(host=setup['IP'], username=setup['cli_user'],
+                                                         password=setup['cli_password'],
                                                          command=sendCommand, return_output=True)
     if (password1 in my_return_status):
         error_list.append(password1)
 
-    # Verify there are no plaintext passwords
-    sendCommand = 'cat ' + filePath + infoLogFile + ' | grep \'' + password2 + '\''
-    my_return_status = af_support_tools.send_ssh_command(host=ipaddress, username=cli_username, password=cli_password,
+    # Verify there are no Password #2
+    sendCommand = 'docker ps | grep ' + service + ' | awk \'{system("docker exec -i "$1" cat ' + filePath + infoLogFile + ' | grep ' + password2 + '")}\''
+    my_return_status = af_support_tools.send_ssh_command(host=setup['IP'], username=setup['cli_user'],
+                                                         password=setup['cli_password'],
                                                          command=sendCommand, return_output=True)
     if (password2 in my_return_status):
         error_list.append(password2)
 
-    # Verify there are no plaintext passwords
-    sendCommand = 'cat ' + filePath + infoLogFile + ' | grep \'' + password3 + '\''
-    my_return_status = af_support_tools.send_ssh_command(host=ipaddress, username=cli_username, password=cli_password,
+    # Verify there are no Password #3
+    sendCommand = 'docker ps | grep ' + service + ' | awk \'{system("docker exec -i "$1" cat ' + filePath + infoLogFile + ' | grep ' + password3 + '")}\''
+    my_return_status = af_support_tools.send_ssh_command(host=setup['IP'], username=setup['cli_user'],
+                                                         password=setup['cli_password'],
                                                          command=sendCommand, return_output=True)
     if (password3 in my_return_status):
         error_list.append(password3)
 
-    assert not error_list, 'Plain-Text Passwords in log files, Review the ' + infoLogFile + ' file'
+    assert not error_list, 'Plain text passwords in log files, Review the ' + infoLogFile + ' file'
 
-    print('No plain text passwords in log files\n')
+    print(
+        '\nNo plain text passwords in log files\n')
 
 ##############################################################################################
