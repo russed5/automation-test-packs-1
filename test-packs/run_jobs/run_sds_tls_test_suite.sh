5fc19f094fce8554689a8bbed1ced305ab17f636
chiranjitxyz26 <chiranjit.sengupta@dell.com>
2017-11-16 15:12:37 +0530

    Update run_sds_tls_test_suite.sh
    Added ESTS-137472 script

--- test-packs/run_jobs/run_sds_tls_test_suite.sh
+++ test-packs/run_jobs/run_sds_tls_test_suite.sh
@@ -2,12 +2,11 @@
 
 TEST_SUITE_NAME='TLS test for System definition service'
 REPORT_NAME='tls_enabled_sds_report'
-SEARCH_PATH=$AF_TEST_SUITE_PATH/continuous-integration-deploy-suite/core-services-ci-cd/c_SystenDefinition/
+SEARCH_PATH=$AF_TEST_SUITE_PATH/
 MARKER='sds_tls_enabled'
 
 . $HOME/af_env.sh
 py3clean .
 export AF_TEST_SUITE_NAME=$TEST_SUITE_NAME
 py.test $SEARCH_PATH -m "$MARKER" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
-py.test $SEARCH_PATH -m "tls_enabled_stop_start" --html $AF_REPORTS_PATH/all/$REPORT_NAME.html --self-contained-html --json $AF_REPORTS_PATH/all/$REPORT_NAME.json --junit-xml $AF_REPORTS_PATH/all/$REPORT_NAME.xml
 export AF_TEST_SUITE_NAME='Test Suite Name Not Set'
