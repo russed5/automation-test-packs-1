93bffba1079e86473e7aef45ec6f1162d6d3edc5
akhtat1 <toqeer.akhtar@vce.com>
2017-11-21 16:48:27 +0000

    Update test_consul_config.py

--- test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 # Author: cullia
 # Revision: 1.1
+# Refactorr by : Toqeer Akhtar
 # Code Reviewed by:
-# Description: Testing the Consul Container.
+# Description: Testing services registered with consul.
 #
 # Copyright (c) 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
 # Dell EMC Confidential/Proprietary Information
-#
+
 import pytest
 import requests
 import af_support_tools
+import socket
+import json
+
+@pytest.fixture(scope="session", params=["amqp", "consul", "api-gateway", "capability-registry-service", "credential-service",
+                                        "endpoint-registration-service", "identity-service", "pam-service", "postgres"])
+def service_name(setup, request):
+    ''' Fixture to get Service names expected to register in Consul'''
+
+    service = request.param
+    print(service)
+    yield service
+
 
 @pytest.mark.daily_status
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
-def test_consul_is_running(hostConnection):
+def test_consul_status(setup):
     """
     Test Case Title :       Verify Consul Service is running
     Description     :       This method tests that consul docker container is running
@@ -23,14 +36,11 @@ def test_consul_is_running(hostConnection):
     Parameters      :       none
     Returns         :       None
     """
-    print('\n* * * Testing Consul on system:', hostConnection.ipAddress, '* * *\n')
-
     service_name = 'consul'
 
     ssh_command = "docker ps --filter name=" + service_name + "  --format '{{.Status}}' | awk '{print $1}'"
-    my_return_status = af_support_tools.send_ssh_command(host=hostConnection.ipAddress, username=hostConnection.username,
-                                                         password=hostConnection.password,
-                                                         command=ssh_command, return_output=True)
+    my_return_status = af_support_tools.send_ssh_command(host = setup['IP'], username= setup['user'], password=setup['password'],
+                                                             command=ssh_command, return_output=True)
     my_return_status = my_return_status.strip()
     print('\nDocker Container is:', my_return_status, '\n')
     assert my_return_status == 'Up', (service_name + " not running")
@@ -39,48 +49,51 @@ def test_consul_is_running(hostConnection):
 @pytest.mark.daily_status
 @pytest.mark.core_services_mvp
 @pytest.mark.core_services_mvp_extended
-@pytest.mark.parametrize("service", ["consul", "vault", "api-gateway"])
-def test_service_is_registered_with_consul(hostConnection, service):
+
+def test_service_is_registered_with_consul(service_name):
     """
-    Test Case Title :       Verify Consul is registered with Consul
-    Description     :       This method tests that consul is registered in the Consul API http://{SymphonyIP}:8500/v1/agent/services
+    Test Case Title :       Verify consul, CoreServices and hdp adapters are registered with Consul
+    Description     :       This method tests that above services is registered in the Consul API http://{SymphonyIP}:8500/v1/agent/services
                             It will fail if :
                                 The line 'Service: "consul"' is not present
     Parameters      :       none
     Returns         :       None
-    Note            :       It's expected that Consul has no Status so there is no Status test.
+
     """
+    print (test_consul_status.__doc__)
+    consulHost = 'consul.cpsd.dell'
+    hostname = socket.gethostname()
+    err = []
 
-    url = 'http://' + hostConnection.ipAddress + ':8500/v1/agent/services'
-    print('sending GET request:', url)
-    response = requests.get(url)
-
-    # Create the string as it should appear in the API
-    service_to_check = '"Service": "' + service + '"'
-
-    assert response.status_code == 200
-    assert service_to_check in response.text, ('ERROR:', service, 'is not in Consul\n')
+    consul_url = 'https://' + consulHost + ':8500/v1/catalog/services'
+    print('sending GET request:', consul_url)
+    response = requests.get(consul_url, verify= '/usr/local/share/ca-certificates/' + hostname + '.ca.crt')
+    data = json.loads(response.text)
 
 
-@pytest.mark.daily_status
-@pytest.mark.core_services_mvp
-@pytest.mark.core_services_mvp_extended
-@pytest.mark.parametrize("service", ["vault", "api-gateway"])
-def test_service_status_is_healthy_in_consul(hostConnection, service):
-    """
-    Test Case Title :       Verify Vault is Passing in Consul
-    Description     :       This method tests that vault has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/vault
-                            It will fail if :
-                                The line '"Status": "passing"' is not present
-    Parameters      :       none
-    Returns         :       None
-    """
+    assert response.status_code == 200, "Error---Request has not been acknowledged as expected"
+    assert service_name in data, 'ERROR--- Service is not Registered in Consul\n'
 
-    url = 'http://' + hostConnection.ipAddress + ':8500/v1/health/checks/' + service
-    print('GET:', url)
 
-    response = requests.get(url)
-    service_status = '"Status": "passing"'
-
-    assert response.status_code == 200
-    assert service_status in response.text, ('ERROR:', service, 'is not Passing in Consul\n')
+# @pytest.mark.daily_status
+# @pytest.mark.core_services_mvp
+# @pytest.mark.core_services_mvp_extended
+# @pytest.mark.parametrize("service", ["vault", "api-gateway"])
+# def test_service_status_is_healthy_in_consul(hostConnection, service):
+#     """
+#     Test Case Title :       Verify Vault is Passing in Consul
+#     Description     :       This method tests that vault has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/vault
+#                             It will fail if :
+#                                 The line '"Status": "passing"' is not present
+#     Parameters      :       none
+#     Returns         :       None
+#     """
+#
+#     url = 'http://' + hostConnection.ipAddress + ':8500/v1/health/checks/' + service
+#     print('GET:', url)
+#
+#     response = requests.get(url)
+#     service_status = '"Status": "passing"'
+#
+#     assert response.status_code == 200
+#     assert service_status in response.text, ('ERROR:', service, 'is not Passing in Consul\n')
