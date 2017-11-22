3bb88bc081971546c21008669afb3142e658f856
akhtat1 <toqeer.akhtar@vce.com>
2017-11-22 11:53:06 +0000

    Update test_consul_config.py

--- test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/core-services-ci-cd/consul/test_consul_config.py
@@ -14,7 +14,7 @@ import af_support_tools
 import socket
 import json
 
-@pytest.fixture(scope="session", params=["amqp", "consul", "api-gateway", "capability-registry-service", "credential-service",
+@pytest.fixture(scope="session", params=["amqp", "api-gateway", "capability-registry-service", "credential-service",
                                         "endpoint-registration-service", "identity-service", "pam-service", "postgres"])
 def service_name(setup, request):
     ''' Fixture to get Service names expected to register in Consul'''
@@ -73,27 +73,31 @@ def test_service_is_registered_with_consul(service_name):
 
     assert response.status_code == 200, "Error---Request has not been acknowledged as expected"
     assert service_name in data, 'ERROR--- Service is not Registered in Consul\n'
+    assert "consul" in data, 'ERROR--- consul is not Registered in Consul\n'
 
 
-# @pytest.mark.daily_status
-# @pytest.mark.core_services_mvp
-# @pytest.mark.core_services_mvp_extended
-# @pytest.mark.parametrize("service", ["vault", "api-gateway"])
-# def test_service_status_is_healthy_in_consul(hostConnection, service):
-#     """
-#     Test Case Title :       Verify Vault is Passing in Consul
-#     Description     :       This method tests that vault has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/vault
-#                             It will fail if :
-#                                 The line '"Status": "passing"' is not present
-#     Parameters      :       none
-#     Returns         :       None
-#     """
-#
-#     url = 'http://' + hostConnection.ipAddress + ':8500/v1/health/checks/' + service
-#     print('GET:', url)
-#
-#     response = requests.get(url)
-#     service_status = '"Status": "passing"'
-#
-#     assert response.status_code == 200
-#     assert service_status in response.text, ('ERROR:', service, 'is not Passing in Consul\n')
+@pytest.mark.daily_status
+@pytest.mark.core_services_mvp
+@pytest.mark.core_services_mvp_extended
+def test_service_status_is_healthy_in_consul(service_name):
+    """
+    Test Case Title :       Verify all services listed in the fixture are Passing in Consul
+    Description     :       This method tests that vault has a passing status in the Consul API http://{SymphonyIP}:8500/v1/health/checks/vault
+                            It will fail if :
+                                The line '"Status": "passing"' is not present
+    Parameters      :       none
+    Returns         :       None
+    """
+
+    print(test_service_status_is_healthy_in_consul.__doc__)
+    consulHost = 'consul.cpsd.dell'
+    hostname = socket.gethostname()
+
+    consul_url = 'https://' + consulHost + ':8500/v1/health/checks/' + service_name
+    print('GET:', consul_url)
+
+    response = requests.get(consul_url, verify='/usr/local/share/ca-certificates/' + hostname + '.ca.crt')
+    service_status = '"Status":"passing"'
+
+    assert response.status_code == 200, "Error---Request has not been acknowledged as expected"
+    assert service_status in response.text, ('ERROR:', service_name, 'is not Passing in Consul\n')
