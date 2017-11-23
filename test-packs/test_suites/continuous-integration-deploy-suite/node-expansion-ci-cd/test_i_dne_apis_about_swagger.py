61a115456db426c9cd156469f3839a1aac5a54fa
Aoife Cullinane <aoife.cullinane@vce.com>
2017-11-23 11:15:19 +0000

    ESTS-137853: Updating DNE API Gateway tests

--- test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_i_dne_apis_about_swagger.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/node-expansion-ci-cd/test_i_dne_apis_about_swagger.py
@@ -79,10 +79,9 @@ def test_node_expansion_api_local_dne(setup, dne_url_body, expected_response):
         raise Exception(err)
 
 
-@pytest.mark.skip(reason='Defect: ESTS-137853')
 @pytest.mark.parametrize('dne_url_body, expected_response', [
-    ('/dne-paqx/dne/about', 'Node Expansion API v0.1'),
-    ('/dne-paqx/swagger-ui.html#', '<!DOCTYPE html>')])
+    ('/node-expansion-service/dne/about', 'Node Expansion API v0.1'),
+    ('/node-expansion-service/swagger-ui.html#', '<!DOCTYPE html>')])
 @pytest.mark.daily_status
 @pytest.mark.dne_paqx_parent_mvp
 @pytest.mark.dne_paqx_parent_mvp_extended
