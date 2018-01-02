36bcf6ff2ab4764c62380845f189bf4b6f511a6a
Mike Maliska <michael.maliska@vce.com>
2018-01-02 13:10:53 -0500

    using new af support tools api

--- test-packs/test_suites/continuous-integration-deploy-suite/network-services-ci-cd/test_nsa_rackhd_adapter_service.py
+++ test-packs/test_suites/continuous-integration-deploy-suite/network-services-ci-cd/test_nsa_rackhd_adapter_service.py
@@ -365,8 +365,7 @@ def test_consul_verify_rackHD_registered():
 
         # Check the response code
         print('Return Code: %s' % url_response[0])
-        assert url_response[0] == '200', 'Return Code NOT 200'
-
+        assert url_response[0] == '200', ('Return Code NOT 200')
         the_response = url_response[1]
 
         # Create the sting as it should appear in the API
